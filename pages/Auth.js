3d1f0603823ae5135d9a7605b5d3d852f39234f1
aarondon1 <issaarony@gmail.com>
2024-09-05 17:25:38 -0400

    basic landingpage and structure of app

---
+++ pages/Auth.js
@@ -0,0 +1,11 @@
+import Register from "@/components/register";
+
+const RegisterPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
